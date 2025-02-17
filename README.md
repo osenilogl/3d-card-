cbea49889019526d03f52f0bf1071d9c216be1d8
osenilogl <raizel343@gmail.com>
2025-02-17 15:41:48 +0100

    help

--- README.md
+++ README.md
@@ -1,36 +1,11 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+HELLO!! i'm using next.js for this website:3
 
-## Getting Started
+i made a website where if u scroll down the cards will turn the other way to show u what u want them to(u just have to configure them lolzies)
 
-First, run the development server:
+if u have any questions about this code, feel free to ask me. 
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+i'm still a beginner at coding so if u have any ways that can help me or if u see any flaws in my cody please tell me:)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+i have not tested it on other devices, only on my pc so it might look weird on different resolutions!
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+u can run the website using "npm run dev"
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,125 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useRef } from "react";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+
+import Card from "./Card";
+
+import ReactLenis from "@studio-freight/react-lenis";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+export default function Home() { 
+  const container = useRef(null);
+  const cardRefs = useRef([]);
+
+
+  useGSAP(() =>{
+    const cards = cardRefs.current;
+    const totalScrollHeight = window.innerHeight * 3;
+    const positions = [14, 38, 62, 86];
+    const rotations = [-15, -7.5, 7.5, 15];
+
+    //pin cards section
+    ScrollTrigger.create({
+      trigger: container.current.querySelector(".cards"),
+      start: "top top",
+      end: ()=> `+=${totalScrollHeight}`,
+      pin: true,
+      pinSpacing: true,
+
+    });
+
+    // spread cards
+
+    cards.forEach((card, index) => {
+      gsap.to(card, {
+        left: `${positions[index]}%`,
+        rotation: `${rotations[index]}`,
+        ease:"none",
+        scrollTrigger: {
+          trigger: container.current.querySelector(".cards"),
+          start: "top top",
+          end: ()=> `+=${window.innerHeight}`,
+          scrub: 0.5,
+          id: `spread-${index}`,
+        },
+      });
+    });
+
+    //flip cards and reset rotation with stragger
+    cards.forEach((card, index) =>{
+      const frontEl = card.querySelector(".flip-card-front");
+      const backEl = card.querySelector(".flip-card-back");
+
+      const straggerOffset = index * 0.05;
+      const startOffset = 1 / 3 + straggerOffset;
+      const endOffset= 2 / 3 + straggerOffset;
+
+      ScrollTrigger.create({
+        trigger: container.current.querySelector(".cards"),
+        start: "top top",
+        end: () => `+=${totalScrollHeight}`,
+        scrub: 1,
+        id:`rotate-flip-${index}`,
+        onUpdate: (self) => {
+          const progress = self.progress;
+          if (progress >= startOffset && progress <= endOffset) {
+            const animationProgress = (progress - startOffset)/ (1 / 3);
+            const frontRotation = -180 * animationProgress;
+            const backRotation = 180 - 180 * animationProgress;
+            const cardRotation = rotations[index] * (1 - animationProgress);
+
+            gsap.to(frontEl, { rotateY: frontRotation, ease: "power1.out"});
+            gsap.to(backEl, {rotateY: backRotation, ease: "power1.out"});
+            gsap.to(card, {
+              xPercent: -50,
+              yPercent: -50,
+              rotate: cardRotation,
+              ease: "power1.out",
+            });
+          }
+        }
+      });
+    });
+
+  },
+  {scope: container}
+);
+
+
+  return<>
+
+  <ReactLenis root>
+  <div className="container" ref={container}>
+
+    <section className="hero">
+
+      <h1>keep scrolling <br /> to reveal the cards</h1>
+      
+     </section>
+
+    <section className="cards">
+      {[...Array(4)].map((_, index)=> (
+        <Card
+        key={index}
+        id={`card-${index + 1} `}
+        frontSrc="/card-front.png"
+        frontAlt="Card Image"
+        backText="."
+        ref={(el) => (cardRefs.current[index] = el)}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      ))}
+    </section>
+    <section className="footer">
+      <h1>footer or upcoming section</h1>
+    </section>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  </div>
+  </ReactLenis>
+  </>;
 }
